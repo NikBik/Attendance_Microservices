18dd0ad66e1729911cc6cef3ba5c322042321ada
Niket <niketsourabh767@gmail.com>
2024-03-24 09:18:29 +0530

    Comments added

--- GatewayServer_Microservice/src/main/java/com/Attendance/Security/SecurityConfig.java
+++ GatewayServer_Microservice/src/main/java/com/Attendance/Security/SecurityConfig.java
@@ -12,17 +12,30 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-		
-		http
-			.authorizeExchange()
-			.anyExchange()
-			.authenticated()
-			.and()
-			.oauth2Client()
-			.and()
-			.oauth2ResourceServer()
-			.jwt();
-		
+
+		http.authorizeExchange().anyExchange().authenticated().and()
+		.oauth2Client() /*
+						 * allows the application to act as
+						 * an OAuth 2.0 client by performing
+						 * operations like obtaining access
+						 * tokens from an authorization
+						 * server.
+						 */
+				.and().oauth2ResourceServer().jwt();
+		/*
+		 * This method enables and configures the OAuth 2.0 resource server support in
+		 * the application. A resource server is responsible for serving protected
+		 * resources to clients that provide valid access tokens. In this case, the
+		 * resource server is configured to validate JSON Web Tokens (JWT) as access
+		 * tokens.
+		 */
+		/*
+		 * .authorizeExchange(exchanges -> exchanges
+		 * .pathMatchers("/public/**").permitAll()
+		 * .pathMatchers("/admin/**").hasRole("ADMIN") .anyExchange().authenticated() //
+		 * Require authentication for all other requests )
+		 */
+
 		return http.build();
 	}
 }
--- ProcessSwipes_Microservice/src/main/java/com/Attendance/Process_UserSwipes_Microservice/KafkaConfig.java
+++ ProcessSwipes_Microservice/src/main/java/com/Attendance/Process_UserSwipes_Microservice/KafkaConfig.java
@@ -13,6 +13,12 @@ public class KafkaConfig {
 	
 	public NewTopic attendanceTopic() {
 		return TopicBuilder.name(topicName).build();
+		/*return new NewTopic(topicName, 5, 3);
+		 * 5 - 5 partitions of topic will be created
+		 * 3- for each partition, 10 replicas are distributed across available brokers
+		 * It means, that within available brokers in your cluster, 15 replicas of topic will be created.
+		 * If I host Kafka on 2 servers, these 15 replicas will be shared across 2 servers    
+		 * */
 	}
 
 	public String getTopicName() {
