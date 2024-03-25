98a0e2e0163697cc296568dfb863631f1c96cf69
Niket <niketsourabh767@gmail.com>
2024-03-25 09:46:06 +0530

    Checking commit

--- ConfigServer_Microservice/src/main/java/com/Attendance/SpringCloudConfigServer/SpringCloudConfigServerApplication.java
+++ ConfigServer_Microservice/src/main/java/com/Attendance/SpringCloudConfigServer/SpringCloudConfigServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
+		//Test comment
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 
